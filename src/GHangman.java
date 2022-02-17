b4e3bf34fd30cc5a74e3a514ae5e967076477f80
Ben <benjamin.b.blodgett@gmail.com>
2022-02-17 12:49:19 -0700

    Finished GHangman

--- src/GHangman.java
+++ src/GHangman.java
@@ -1,8 +1,11 @@
 import acm.graphics.GCompound;
 import acm.graphics.GLine;
 import acm.graphics.GObject;
+import acm.graphics.GOval;
 
+import java.awt.*;
 import java.util.ArrayList;
+import svu.csc213.Dialog;
 
 public class GHangman extends GCompound {
     ArrayList<GObject> b = new ArrayList<>();
@@ -15,5 +18,43 @@ public class GHangman extends GCompound {
         add(o);
         GLine w = new GLine(30,-60,30,-30);
         add(w);
+    }
+    public void reset(){
+        while(b.size()>0){
+            remove(b.get(0));
+            b.remove(0);
+        }
+    }
+    public void add(){
+        GObject v = null;
+        switch (b.size()){
+            case 0 ->{
+                v = new GOval(15,15);
+                v.setLocation(30-v.getWidth()/2,-30);
+            }
+            case 1 -> v = new GLine(30,-15,30,0);
+            case 2 -> v = new GLine(30,-10,15,-20);
+            case 3 -> v = new GLine(30,-10,45,-20);
+            case 4 -> v = new GLine(30,-5,15,5);
+            case 5 -> v = new GLine(30,-5,45,5);
+            case 6 ->{
+                v = new GOval(5,5);
+                ((GOval)v).setFilled(true);
+                ((GOval)v).setFillColor(Color.black);
+                v.setLocation(25,-25);
+            }
+            case 7 ->{
+                v = new GOval(5,5);
+                ((GOval)v).setFilled(true);
+                ((GOval)v).setFillColor(Color.black);
+                v.setLocation(35,-25);
+            }
+            default -> {
+                Dialog.showMessage("You died.");
+                System.exit(0);
+            }
+        }
+        add(v);
+        b.add(v);
     }
 }
--- src/Hangman.java
+++ src/Hangman.java
@@ -6,5 +6,11 @@ public class Hangman extends GraphicsProgram {
     }
     public void run(){
         add(new GHangman(),100,100);
+        for (int i = 0; i < 8; i++) {
+            waitForClick();
+            ((GHangman)getElement(0)).add();
+        }
+        ((GHangman)getElement(0)).reset();
+
     }
 }
