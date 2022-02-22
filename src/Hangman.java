7f6ad386e736c352d4ae1dad52adc9318c6fea71
Ben <benjamin.b.blodgett@gmail.com>
2022-02-22 12:45:32 -0700

    added i to the alphabet
    i wasn't in the alphabet

--- src/Hangman.java
+++ src/Hangman.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hangman extends GraphicsProgram {
-    static char[] letters = new char[]{'a','b','c','d','e','f','g','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    static char[] letters = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     static ArrayList<Character> guesses = new ArrayList<>();
     static ArrayList<Character> word = new ArrayList<>();
     static ArrayList<String> words = new ArrayList<>();
